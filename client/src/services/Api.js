78194ab3ff150a7ac8d9de7e40dddfe1180e05d8
giorgioindelicato11 <giorgioindelicato11@gmail.com>
2021-11-04 16:55:41 +0100

    fixed

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
+
 export default () => {
-    return axios.create({
-        baseUrl : `http://localhost:8081/`
-    })
+  return axios.create({
+    baseURL: `http://localhost:8081/`
+  })
 }
